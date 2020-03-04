2f0e9636ab04d8d17f91c598630543a3ecc60b8d
FabienCst <fabien_castilla@outlook.fr>
2020-03-04 01:49:50 +0100

    Modification design de l'application. Création des fragments "Accueil" et "Magasins". Modification de la classe "MagasinAdapter" (désormais affichage dynamique). Création de la classe "PromotionsAdapter".

--- app/src/main/java/com/example/webuy/ui/promotions/PromotionsViewModel.java
+++ app/src/main/java/com/example/webuy/ui/promotions/PromotionsViewModel.java
@@ -1,16 +1,16 @@
-package com.example.webuy.ui.gallery;
+package com.example.webuy.ui.promotions;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class PromotionsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public PromotionsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("Ici, promotions fragment.");
     }
 
     public LiveData<String> getText() {
