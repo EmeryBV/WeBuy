bbe34d43ddb8faa0ec79e30116c5454ea12f6ffc
EmeryBV <emery.vecchio@gmail.com>
2020-03-22 21:12:32 +0100

    Début sign in

--- app/src/main/java/com/appsnipp/schooleducation/LoginActivity2.java
+++ app/src/main/java/com/appsnipp/schooleducation/LoginActivity2.java
@@ -3,6 +3,8 @@ package com.appsnipp.schooleducation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.EditText;
 
 public class LoginActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class LoginActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+        EditText password = (EditText) findViewById(R.id.password);
+        password.setTransformationMethod(new PasswordTransformationMethod());
     }
 }
