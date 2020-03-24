af39beba7050424197b268681770d662cb149257
EmeryBV <57750500+EmeryBV@users.noreply.github.com>
2020-03-24 14:41:01 +0100

    Changement accueil

--- app/src/main/java/com/appsnipp/schooleducation/MainActivity.java
+++ app/src/main/java/com/appsnipp/schooleducation/MainActivity.java
@@ -1,10 +1,13 @@
 package com.appsnipp.schooleducation;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.view.menu.MenuView;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -14,6 +17,8 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -22,6 +27,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.appsnipp.schooleducation.ui.magasins.MagasinsFragment;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -58,6 +65,8 @@ public class MainActivity extends AppCompatActivity
                 drawer.openDrawer(GravityCompat.START);
             }
         });
+
+
     }
 
 
@@ -76,7 +85,7 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        boolean nuit = false;
         Fragment fragment = null;
         switch (item.getItemId()) {
             case R.id.nav_home:
@@ -92,6 +101,7 @@ public class MainActivity extends AppCompatActivity
                 startActivity(secondeActivite);
                 break;
             case R.id.dark_mode:
+
                 DarkModePrefManager darkModePrefManager = new DarkModePrefManager(this);
                 darkModePrefManager.setDarkMode(!darkModePrefManager.isNightMode());
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
